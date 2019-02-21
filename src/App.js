b87b61f0d164751b9b7044ce71cc32ebc9b4911e
paulbriar <pbriar@live.com>
2019-02-20 21:41:40 -0400

    Wired up Header & Card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import Header from './components/Header';
+
 const App = () => {
   return (
     <View>
+      <Header headerText="Auth"/>
       <Text>App Component</Text>
     </View>
   );
